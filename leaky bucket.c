5853533451bfa71e4fba3fe59935ce59aa4562c5
Nova-KO <63781429+Nova-KO@users.noreply.github.com>
2022-09-13 14:03:49 +0530

    Create leaky bucket.c

---
+++ leaky bucket.c
@@ -0,0 +1,15 @@
+#include<stdip.h>
+#include<studio.h>
+#define NOF_PACKETS 10
+int rand(int a)
+{
+ int rn = (random()%10)%a;
+ return rn ==0?1:m;
+ }
+ int main()
+ {
+   int packet sz[NOF_PACKETS],i,clk,b_size,o_rate,p_sz_rm=0,p_sz,p_time,op;
+   for(i=0;i<NOF_PACKET;++i)
+     packet_sz[i]=rand(6)*10;
+   for(i=0;i<NOF_PACKETS;++i)
+   printf()  

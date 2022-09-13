84247d187423fe3f196187ba45f595f9bdcb6c41
Nova-KO <63781429+Nova-KO@users.noreply.github.com>
2022-09-13 14:01:23 +0530

    Create smtp server.c

---
+++ smtp server.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<stdlib.h>
+#include<netinet/in.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#define max 1024
+int fdl,l;
+struct sockaddr_in server,client;
+transfer()
+{
+ int n,retval,fd;
+ char*data;
+ data=((void*)malloc(max));
+ fd=open("mailbox",O_RDWR||O_APPEND);
+ retval=recvform(fdl,data,max,0,(struct sockaddr *)&client,&l);
+ write(fd,data,retval);
+ printf("\n recevd 1 mail\n");
+}
+main()
+{
+ int l=0;
+ server.sin_family=AF_INET;
+ server.sin_port=htons(6299);
+ server.sin_addr.s_addr=htonl(INADDR_ANY);
+ fdl=socket(AF_INET,SOCK_DGRAM,0);
+ l=sizeof(client);
+ binnd(fdl,(struct sockaddr *)&server,1 );
+ transfer();
+}
+ 
